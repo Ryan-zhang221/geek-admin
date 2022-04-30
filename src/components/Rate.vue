12fb1cfd9ab057a97c7a5b3e7458fed438eca6a9
ryan <>
2022-05-01 00:08:52 +0800

    update：支持小数点评分

--- src/components/Rate.vue
+++ src/components/Rate.vue
@@ -1,11 +1,16 @@
 <template>
   <div :style="fontstyle">
-    {{rate}}
+    <div class="rate" @mouseout="mouseOut">
+      <span @mouseover="mouseOver(num)" v-for="num in 5" :key="num">☆</span>
+      <span class="hollow" :style="fontwidth">
+        <span @click="onRate(num)" @mouseover="mouseOver(num)" v-for="num in 5" :key="num">★</span>
+      </span>
+    </div>
   </div>
 </template>
 
 <script setup>
-  import { defineProps, computed } from 'vue'
+  import { ref, computed, defineEmits } from 'vue'
   let props = defineProps({
     value: Number,
     theme: { type: String, default: 'orange' }
@@ -28,4 +33,29 @@
   const fontstyle = computed(() => {
     return `color: ${themeObj[props.theme]}`
   })
+
+  // 评分宽度
+  let width = ref(props.value)
+  function mouseOver(i) {
+    width.value = i
+  }
+  function mouseOut() {
+    width.value = props.value
+  }
+  const fontwidth = computed(() => {
+    return `width: ${width.value}em;`
+  })
+
+  // 事件
+  let emits = defineEmits('update-rate')
+  
+  function onRate(num) {
+    emits('update-rate', num)
+  }
+
 </script>
+
+<style scoped>
+.rate{ position:relative; display: inline-block;}
+.rate > span.hollow { position:absolute; display: inline-block; top:0; left:0; width:0; overflow:hidden;}
+</style>
--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,9 +1,9 @@
 <template>
   <h2>这是关于页面</h2>
   <button @click="toggle">全屏切换</button>
-  <Rate :value="5"></Rate>
-  <Rate :value="score" theme="green"></Rate>
-  <Rate :value="score" theme="yellow"></Rate>
+
+  <h5>你的评分是：{{score}}</h5>
+  <Rate :value="score" @update-rate="update"></Rate>
 </template>
 
 
@@ -16,5 +16,8 @@
 
   const { isFullscreen, enter, exit, toggle } = useFullscreen()
 
-  let score = ref(2)
+  let score = ref(2.5)
+  function update(num) {
+    score.value  = num
+  }
 </script>
