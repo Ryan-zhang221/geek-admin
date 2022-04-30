b94774680f095518c59498b2ef1006fd28952344
ryan <>
2022-04-30 23:34:52 +0800

    update：增加评分组件

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,14 +1,20 @@
 <template>
   <h2>这是关于页面</h2>
   <button @click="toggle">全屏切换</button>
+  <Rate :value="score"></Rate>
+  <Rate :value="score" theme="green"></Rate>
+  <Rate :value="score" theme="yellow"></Rate>
 </template>
 
+
+
 <script setup>
-  import { useFavicon } from '../utils/use';
+  import { ref } from 'vue'
+  import Rate from '../components/Rate.vue'
+
   import { useFullscreen } from '@vueuse/core'
-  let { favicon } = useFavicon()
-  function loading() {
-    favicon.value = '/assets/close.png'
-  }
+
   const { isFullscreen, enter, exit, toggle } = useFullscreen()
+
+  let score = ref(2)
 </script>
