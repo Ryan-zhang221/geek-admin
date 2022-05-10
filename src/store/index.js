dc3b9d5bf96aa894683b4cbb005a21c351320d9e
zhangteng <zhangteng@q1.com>
2022-05-10 16:08:57 +0800

    update:minivuex实现

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex'
+// import { createStore } from './gvuex.js'
 
 // 我们使用createStore来创建一个数据存储，叫做store，store内部除了数据state，还需要一个
 // mutation配置去修改数据，可以把这个mutation理解为数据更新的【申请单】，mutation内部的
