0117566319209db09a25392569606c6a2e57d502
ryan <>
2022-06-12 11:19:30 +0800

    update：test

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,19 +1,21 @@
 <template>
-  <h2>这是关于页面</h2>
-  <button @click="toggle">全屏切换</button>
+  <div>
+    <h2>这是关于页面</h2>
+    <button @click="toggle">全屏切换</button>
 
-  <h5>你的评分是：{{score}}</h5>
-  <Rate :value="score" @update-rate="update" theme="green"></Rate>
-  <Rate :value="score" @update-rate="update" theme="white"></Rate>
-  <Rate :value="score" @update-rate="update" theme="blue"></Rate>
-  <Rate :value="score" @update-rate="update"></Rate>
-  <Rate :value="score" @update-rate="update">
-    <img src="../assets/logo.png" alt="" width="14">
-  </Rate>
-  <Count></Count>
+    <h5>你的评分是：{{score}}</h5>
+    <Rate :value="score" @update-rate="update" theme="green"></Rate>
+    <Rate :value="score" @update-rate="update" theme="white"></Rate>
+    <Rate :value="score" @update-rate="update" theme="blue"></Rate>
+    <Rate :value="score" @update-rate="update"></Rate>
+    <Rate :value="score" @update-rate="update">
+      <img src="../assets/logo.png" alt="" width="14">
+    </Rate>
+    <Count></Count>
 
-  <!-- level为h1~h6级标签 -->
-  <Heading :level="1">hello world</Heading>
+    <!-- level为h1~h6级标签 -->
+    <Heading :level="1">hello world</Heading>
+  </div>
 
 </template>
 
@@ -27,7 +29,7 @@
 
   import { useFullscreen } from '@vueuse/core'
 
-  const { isFullscreen, enter, exit, toggle } = useFullscreen()
+  const { toggle } = useFullscreen()
 
   let score = ref(3.5)
   function update(num) {
